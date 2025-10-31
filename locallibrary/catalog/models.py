f56475b10106b6513bb52a4974632824b88d3a01
ellieatkins1 <atkin2il@dukes.jmu.edu>
2025-10-31 14:37:18 -0400

    Authentication and Permissions

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 from django.urls import reverse # Used in get_absolute_url() to get URL for specified ID
 
+from datetime import date
 from django.db.models import UniqueConstraint # Constrains fields to unique values
 from django.db.models.functions import Lower # Returns lower cased value of field
+from django.conf import settings
 
 class Genre(models.Model):
     """Model representing a book genre."""
@@ -114,6 +116,13 @@ class BookInstance(models.Model):
         default='m',
         help_text='Book availability',
     )
+
+    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
 
     class Meta:
         ordering = ['due_back']
